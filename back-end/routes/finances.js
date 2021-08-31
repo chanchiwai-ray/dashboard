fe1f5a8db4773dd16eab86305a8981cbca9fd978
Chi Wai, Chan <chanchiwairay@gmail.com>
2021-08-31 18:32:38 +0900

    backend/finances: add /:uid/records/daily endpoint to fetch daily records. Note, this is different from /:uid/records/ which fetch raw records.

--- back-end/routes/finances.js
+++ back-end/routes/finances.js
@@ -62,6 +62,91 @@ router
   .delete((req, res) => {
     res.status(501).json({ error: "DELETE is not implemented." });
   });
+
+// route to get the of users:id's finance records grouped by date from start to end
+router
+  .route("/:uid/records/daily")
+  .get(authenticate.isSameUser, (req, res) => {
+    const start = Math.round(Number(req.query.start)) || 0;
+    const end = Math.round(Number(req.query.end)) || 100 * 365 * 24 * 60 * 60 * 1000;
+    Records.aggregate([
+      {
+        $match: {
+          date: {
+            $gte: `${start}`,
+            $lte: `${end}`,
+          },
+        },
+      },
+      {
+        $addFields: {
+          createdAt: {
+            $toDate: {
+              $toLong: "$date",
+            },
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            year: {
+              $year: "$createdAt",
+            },
+            month: {
+              $month: "$createdAt",
+            },
+            day: {
+              $dayOfMonth: "$createdAt",
+            },
+            category: "$category",
+          },
+          categoryTotal: {
+            $sum: "$amount",
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            year: "$_id.year",
+            month: "$_id.month",
+            day: "$_id.day",
+          },
+          dailyTotal: {
+            $sum: "$categoryTotal",
+          },
+          categorySubTotal: {
+            $push: {
+              category: "$_id.category",
+              total: "$categoryTotal",
+            },
+          },
+        },
+      },
+    ])
+      .then((sums) => {
+        res.status(200).json({
+          success: true,
+          message: `returned ${sums.length} results.`,
+          payload: sums,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({ success: false, message: "GET not success.", payload: null });
+        console.log(err);
+      });
+  })
+  .post((req, res) => {
+    res.status(501).json({ error: "POST is not implemented." });
+  })
+  .put((req, res) => {
+    res.status(501).json({ error: "PUT is not implemented." });
+  })
+  .delete((req, res) => {
+    res.status(501).json({ error: "DELETE is not implemented." });
+  });
+
 // route to users:id's all finance records
 router
   .route("/:uid/records")
