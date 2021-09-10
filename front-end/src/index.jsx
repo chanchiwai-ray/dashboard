36d2db3ad26e9f8c59b904ab9b8d918fcd2bd80f
Chi Wai, Chan <chanchiwairay@gmail.com>
2021-09-10 15:57:17 +0900

    frontend: persist login session with redux-persist.

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
+import { Provider, useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import DashboardPage from "./views/DashboardPage.jsx";
@@ -10,8 +13,9 @@ import LoginPage from "./views/LoginPage.jsx";
 
 import "./index.css";
 import { selectAuth, store } from "./redux/app/store.js";
-import { Provider, useDispatch, useSelector } from "react-redux";
 import { verify } from "./redux/slices/common/auth.js";
+
+const persistor = persistStore(store);
 
 const PrivateRoute = ({ component: Component, fallbackPath, ...props }) => {
   const auth = useSelector(selectAuth);
@@ -38,25 +42,27 @@ const PrivateRoute = ({ component: Component, fallbackPath, ...props }) => {
 const Main = (props) => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/login" component={LoginPage} />
-          <PrivateRoute path="/home" component={DashboardPage} />
-          <PrivateRoute path="/profile" component={ProfilePage} />
-          <PrivateRoute path="/expense" component={ExpensePage} />
-          <Redirect from="/" to={window.localStorage.getItem("redirect") || "/login"} />
-          <Route
-            render={() => (
-              <div
-                className="d-flex align-items-center justify-content-center"
-                style={{ width: "100%", height: "100%" }}
-              >
-                <h1>404: Page not found...</h1>
-              </div>
-            )}
-          />
-        </Switch>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/login" component={LoginPage} />
+            <PrivateRoute path="/home" component={DashboardPage} />
+            <PrivateRoute path="/profile" component={ProfilePage} />
+            <PrivateRoute path="/expense" component={ExpensePage} />
+            <Redirect from="/" to={window.localStorage.getItem("redirect") || "/login"} />
+            <Route
+              render={() => (
+                <div
+                  className="d-flex align-items-center justify-content-center"
+                  style={{ width: "100%", height: "100%" }}
+                >
+                  <h1>404: Page not found...</h1>
+                </div>
+              )}
+            />
+          </Switch>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 };
--- front-end/src/redux/app/store.js
+++ front-end/src/redux/app/store.js
@@ -1,21 +1,39 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+
 import authReducer from "../slices/common/auth.js";
 import datesReducer from "../slices/common/dates.js";
 import profileReducer from "../slices/user/profile.js";
 import settingsReducer from "../slices/common/settings.js";
 import recordsReducer from "../slices/finance/records.js";
 import categoriesReducer from "../slices/finance/categories.js";
+import autoMergeLevel1 from "redux-persist/es/stateReconciler/autoMergeLevel1";
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  dates: datesReducer,
+  profile: profileReducer,
+  settings: settingsReducer,
+  records: recordsReducer,
+  categories: categoriesReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel1,
+  whitelist: ["auth", "settings"],
+};
+
+const persistingReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    dates: datesReducer,
-    profile: profileReducer,
-    settings: settingsReducer,
-    records: recordsReducer,
-    categories: categoriesReducer,
-  },
+  reducer: persistingReducer,
+  middleware: [thunk], // required by redux-persist
 });
 
 export const selectAuth = (state) => state.auth;
