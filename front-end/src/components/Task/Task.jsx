5ab3f25f2ed6523b599709eaa673acc7d329e113
Chi Wai, Chan <chanchiwairay@gmail.com>
2021-09-19 22:07:01 +0900

    frontend/Task: refactor a bit.

--- front-end/src/components/Task/Task.jsx
+++ front-end/src/components/Task/Task.jsx
@@ -51,177 +51,175 @@ export default function Task({ content, onDelete, onDone, onCancel, ...props }) 
     return "";
   };
 
+  const renderCheckBox = () => (
+    <OverlayTrigger overlay={<Tooltip>{formik.values.completed ? "UnCheck" : "Check"}</Tooltip>}>
+      <FontAwesomeIcon
+        className={`${styles["fontawesome-as-btn"]} mx-1`}
+        icon={formik.values.completed ? faCheckSquare : faSquare}
+        onClick={() => {
+          formik.setFieldValue("completed", !formik.values.completed);
+          if (!isEditing) {
+            formik.handleSubmit();
+          }
+        }}
+      />
+    </OverlayTrigger>
+  );
+
+  const renderStarEditDelete = () => (
+    <React.Fragment>
+      <OverlayTrigger overlay={<Tooltip>{formik.values.star ? "Unstar" : "Star"}</Tooltip>}>
+        <FontAwesomeIcon
+          className={`${styles["fontawesome-as-btn"]} mx-1`}
+          color="#ff55f2"
+          icon={formik.values.star ? faStarHollow : faStarFilled}
+          onClick={() => {
+            formik.setFieldValue("star", !formik.values.star);
+            if (!isEditing) {
+              formik.handleSubmit();
+            }
+          }}
+        />
+      </OverlayTrigger>
+      {!formik.values.completed ? (
+        <OverlayTrigger overlay={<Tooltip>Edit</Tooltip>}>
+          <FontAwesomeIcon
+            className={`${styles["fontawesome-as-btn"]} mx-1`}
+            color="green"
+            icon={faEdit}
+            onClick={() => isEditing || setEditing(true)}
+          />
+        </OverlayTrigger>
+      ) : null}
+      <OverlayTrigger overlay={<Tooltip>Delete</Tooltip>}>
+        <FontAwesomeIcon
+          className={`${styles["fontawesome-as-btn"]} mx-1`}
+          color="red"
+          icon={faTrash}
+          onClick={() => onDelete()}
+        />
+      </OverlayTrigger>
+    </React.Fragment>
+  );
+
+  const renderEditForm = () => (
+    <Form onSubmit={formik.handleSubmit}>
+      <Form.Row>
+        <Form.Group as={Col} lg={12}>
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            id="title"
+            name="title"
+            onChange={formik.handleChange}
+            value={formik.values.title}
+            required
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group as={Col} lg={12}>
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            id="description"
+            name="description"
+            onChange={formik.handleChange}
+            value={formik.values.description}
+          ></Form.Control>
+        </Form.Group>
+      </Form.Row>
+      <Form.Row>
+        <Form.Group as={Col} lg={6}>
+          <Form.Label>Due Date</Form.Label>
+          <Form.Control
+            type="date"
+            id="dueDate"
+            name="dueDate"
+            onChange={(e) => formik.setFieldValue("dueDate", e.target.value)}
+            value={convertToDateString(formik.values.dueDate)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group as={Col} lg={6}>
+          <Form.Label>Priority</Form.Label>
+          <Form.Control
+            as="select"
+            id="priority"
+            name="priority"
+            onChange={formik.handleChange}
+            value={formik.values.priority}
+          >
+            {[...Array(10).keys()].map((v, i) => (
+              <option key={i}>{v}</option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+      </Form.Row>
+      <div className="d-flex align-items-end justify-content-end">
+        <span className="text-muted mr-auto d-none d-lg-block">
+          Last modified: {new Date(Number(formik.values.modifiedDate)).toLocaleString("en-GB")}
+        </span>
+        <Button type="submit" className="mx-2">
+          Done
+        </Button>
+        <Button
+          type="reset"
+          className="mx-2"
+          variant="secondary"
+          onClick={() => {
+            setEditing(false);
+            if (onCancel) {
+              onCancel();
+            }
+          }}
+        >
+          Cancel
+        </Button>
+      </div>
+    </Form>
+  );
+
+  const renderDisplayTask = () => (
+    <Row>
+      <Col lg={12}>
+        <span className={`${styles["task-title-text"]}`}>{formik.values.title}</span>
+      </Col>
+      <Col lg={12}>
+        <span className={`${styles["task-description-text"]}`}>{formik.values.description}</span>
+      </Col>
+      <Col lg={12}>
+        <span className={`${styles["task-meta-text"]}`}>Priority: {formik.values.priority}</span>
+      </Col>
+      <Col lg={12} className={`${styles["task-meta-text"]}`}>
+        <span>
+          {!formik.values.dueDate
+            ? ""
+            : `Due: ${new Date(Number(formik.values.dueDate)).toLocaleDateString("en-GB", {
+                year: "numeric",
+                month: "numeric",
+                day: "numeric",
+              })}`}
+        </span>
+      </Col>
+    </Row>
+  );
+
   return (
     <Row
       className={`${
         isEditing ? styles["task-item-editing"] : styles["task-item"]
       } align-items-center`}
     >
-      <Col xs={2} className="d-flex justify-content-center">
-        {
-          <OverlayTrigger
-            overlay={<Tooltip>{formik.values.completed ? "UnCheck" : "Check"}</Tooltip>}
-          >
-            <FontAwesomeIcon
-              className={`${styles["fontawesome-as-btn"]}`}
-              icon={formik.values.completed ? faCheckSquare : faSquare}
-              onClick={() => {
-                formik.setFieldValue("completed", !formik.values.completed);
-                if (!isEditing) {
-                  formik.handleSubmit();
-                }
-              }}
-            />
-          </OverlayTrigger>
-        }
+      <Col lg={2} className="d-none d-lg-block text-center">
+        {renderCheckBox()}
       </Col>
-      <Col xs={8}>
-        {isEditing ? (
-          <Form onSubmit={formik.handleSubmit}>
-            <Form.Row>
-              <Form.Label column lg={2}>
-                Title
-              </Form.Label>
-              <Form.Group as={Col} lg={10}>
-                <Form.Control
-                  id="title"
-                  name="title"
-                  onChange={formik.handleChange}
-                  value={formik.values.title}
-                  required
-                ></Form.Control>
-              </Form.Group>
-              <Form.Label column lg={2}>
-                Description
-              </Form.Label>
-              <Form.Group as={Col} lg={10}>
-                <Form.Control
-                  id="description"
-                  name="description"
-                  onChange={formik.handleChange}
-                  value={formik.values.description}
-                ></Form.Control>
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Label column lg={2}>
-                Due Date
-              </Form.Label>
-              <Form.Group as={Col} lg={5}>
-                <Form.Control
-                  type="date"
-                  id="dueDate"
-                  name="dueDate"
-                  onChange={(e) => formik.setFieldValue("dueDate", e.target.value)}
-                  value={convertToDateString(formik.values.dueDate)}
-                ></Form.Control>
-              </Form.Group>
-              <Form.Label column lg={2}>
-                Priority
-              </Form.Label>
-              <Form.Group as={Col} lg={3}>
-                <Form.Control
-                  as="select"
-                  id="priority"
-                  name="priority"
-                  onChange={formik.handleChange}
-                  value={formik.values.priority}
-                >
-                  {[...Array(10).keys()].map((v, i) => (
-                    <option key={i}>{v}</option>
-                  ))}
-                </Form.Control>
-              </Form.Group>
-            </Form.Row>
-            <div className="d-flex align-items-end justify-content-end">
-              <span className="text-muted mr-auto d-none d-lg-block">
-                Last modified:{" "}
-                {new Date(Number(formik.values.modifiedDate)).toLocaleString("en-GB")}
-              </span>
-              <Button type="submit" className="mx-2">
-                Done
-              </Button>
-              <Button
-                type="reset"
-                className="mx-2"
-                variant="secondary"
-                onClick={() => {
-                  setEditing(false);
-                  if (onCancel) {
-                    onCancel();
-                  }
-                }}
-              >
-                Cancel
-              </Button>
-            </div>
-          </Form>
-        ) : (
-          <Row>
-            <Col lg={12}>
-              <Row>
-                <Col lg={12}>
-                  <span className={`${styles["task-title-text"]}`}>{formik.values.title}</span>
-                </Col>
-                <Col lg={12}>
-                  <span className={`${styles["task-description-text"]}`}>
-                    {formik.values.description}
-                  </span>
-                </Col>
-                <Col lg={12}>
-                  <span className={`${styles["task-meta-text"]}`}>
-                    Priority: {formik.values.priority}
-                  </span>
-                </Col>
-                <Col lg={12} className={`${styles["task-meta-text"]}`}>
-                  <span>
-                    {!formik.values.dueDate
-                      ? ""
-                      : `Due: ${new Date(Number(formik.values.dueDate)).toLocaleDateString(
-                          "en-GB",
-                          { year: "numeric", month: "numeric", day: "numeric" }
-                        )}`}
-                  </span>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        )}
+      <Col xs={12} className="d-lg-none d-block text-right">
+        {renderCheckBox()}
+        {renderStarEditDelete()}
+        <hr />
       </Col>
-      <Col xs={2} className="d-flex justify-content-center">
-        <OverlayTrigger overlay={<Tooltip>{formik.values.star ? "Unstar" : "Star"}</Tooltip>}>
-          <FontAwesomeIcon
-            className={`${styles["fontawesome-as-btn"]} mx-1`}
-            color="#ff55f2"
-            icon={formik.values.star ? faStarHollow : faStarFilled}
-            onClick={() => {
-              formik.setFieldValue("star", !formik.values.star);
-              if (!isEditing) {
-                formik.handleSubmit();
-              }
-            }}
-          />
-        </OverlayTrigger>
-        {formik.values.completed ? (
-          <div></div>
-        ) : (
-          <OverlayTrigger overlay={<Tooltip>Edit</Tooltip>}>
-            <FontAwesomeIcon
-              className={`${styles["fontawesome-as-btn"]} mx-1`}
-              color="green"
-              icon={faEdit}
-              onClick={() => isEditing || setEditing(true)}
-            />
-          </OverlayTrigger>
-        )}
-        <OverlayTrigger overlay={<Tooltip>Delete</Tooltip>}>
-          <FontAwesomeIcon
-            className={`${styles["fontawesome-as-btn"]} mx-1`}
-            color="red"
-            icon={faTrash}
-            onClick={() => onDelete()}
-          />
-        </OverlayTrigger>
+      <Col xs={12} lg={8}>
+        {isEditing ? renderEditForm() : renderDisplayTask()}
+      </Col>
+      <Col lg={2} className="d-none d-lg-block text-center">
+        {renderStarEditDelete()}
       </Col>
     </Row>
   );
